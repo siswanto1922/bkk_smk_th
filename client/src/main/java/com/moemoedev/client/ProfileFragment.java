c0498113df9a3aa68f4ca95077a0d28fc12728ca
Pujo Siswanto <pujosiswanto131922@gmail.com>
2020-10-06 09:21:45 +0700

    bismillah

--- client/src/main/java/com/moemoedev/client/ProfileFragment.java
+++ client/src/main/java/com/moemoedev/client/ProfileFragment.java
@@ -1,13 +1,21 @@
 package com.moemoedev.client;
 
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+import static android.content.Context.MODE_PRIVATE;
+import static androidx.constraintlayout.widget.Constraints.TAG;
 
 
 /**
@@ -25,9 +33,20 @@ public class ProfileFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View v = inflater.inflate(R.layout.fragment_profile, container, false);
 
-        
+        final Button btnLogout = v.findViewById(R.id.btnLogout);
+
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getActivity(), "Logout Berhasil", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(getActivity(),AuthActivity.class));
+            }
+        });
+
+        return v;
+
     }
 
 }
