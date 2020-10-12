0c803e8d971cbc123adafc255c1402316f96d1f2
Pujo Siswanto <pujosiswanto131922@gmail.com>
2020-10-12 08:07:12 +0700

    bismillah

---
+++ admin/src/main/java/com/moemoedev/bkksmkthp/DetailLokerActivity.java
@@ -0,0 +1,37 @@
+package com.moemoedev.bkksmkthp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class DetailLokerActivity extends AppCompatActivity {
+
+    TextView detailIndustry, detailDesc, detailDeadline;
+    Button detailCheckApplicant;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_loker);
+
+        detailIndustry = findViewById(R.id.detailActivity_industry);
+        detailDesc = findViewById(R.id.detailActivity_desc);
+        detailDeadline = findViewById(R.id.detailActivity_deadline);
+        detailCheckApplicant = findViewById(R.id.btn_checkApplicant);
+
+        detailIndustry.setText(getIntent().getStringExtra("industry"));
+        detailDesc.setText(getIntent().getStringExtra("desc"));
+        detailDeadline.setText(getIntent().getStringExtra("deadline"));
+
+        detailCheckApplicant.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+    }
+}
