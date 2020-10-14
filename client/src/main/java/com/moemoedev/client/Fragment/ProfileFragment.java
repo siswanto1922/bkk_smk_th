c5e0029ade5c499e2361e83371b8a14f7ce8eeed
Pujo Siswanto <pujosiswanto131922@gmail.com>
2020-10-14 13:36:41 +0700

    bismillah

--- client/src/main/java/com/moemoedev/client/Fragment/ProfileFragment.java
+++ client/src/main/java/com/moemoedev/client/Fragment/ProfileFragment.java
@@ -1,27 +1,45 @@
 package com.moemoedev.client.Fragment;
 
-
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.constraintlayout.solver.widgets.Snapshot;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.moemoedev.client.Activity.AuthActivity;
 import com.moemoedev.client.R;
+
+import java.util.Objects;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+import static android.content.Context.MODE_PRIVATE;
+import static androidx.constraintlayout.widget.Constraints.TAG;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class ProfileFragment extends Fragment {
-
-
     public ProfileFragment() {
         // Required empty public constructor
     }
@@ -31,8 +49,50 @@ public class ProfileFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View v = inflater.inflate(R.layout.fragment_profile, container, false);
+        final View v = inflater.inflate(R.layout.fragment_profile, container, false);
 
+        DatabaseReference user = FirebaseDatabase.getInstance().getReference("User");
+        DatabaseReference childUser;
+        childUser = user.child(Objects.requireNonNull(FirebaseAuth.getInstance().getCurrentUser()).getUid());
+        childUser.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+//                Toast.makeText(getApplicationContext(), snapshot.child("email").getValue().toString(), Toast.LENGTH_SHORT).show();
+
+                try {
+                    TextView isinama = v.findViewById(R.id.isinama);
+                    TextView isinisn = v.findViewById(R.id.isinisn);
+                    TextView isijk = v.findViewById(R.id.isijk);
+                    TextView isijurusan = v.findViewById(R.id.isijurusan);
+                    TextView isitk = v.findViewById(R.id.isitk);
+                    TextView isialamat = v.findViewById(R.id.isialamat);
+                    TextView isitgl = v.findViewById(R.id.isitgl);
+
+                    isinisn.setText(snapshot.child("nisn").getValue().toString());
+                    isijk.setText(snapshot.child("jk").getValue().toString());
+                    isijurusan.setText(snapshot.child("jurusan").getValue().toString());
+                    isitk.setText(snapshot.child("graduate").getValue().toString());
+                    isialamat.setText(snapshot.child("address").getValue().toString());
+                    isitgl.setText(snapshot.child("ttl").getValue().toString());
+                    isinama.setText(snapshot.child("name").getValue().toString());
+
+
+                } catch (Exception e) {
+                    Toast.makeText(getApplicationContext(), ""+e, Toast.LENGTH_SHORT);
+                }
+
+            }
+
+            private Context getApplicationContext() {
+                return null;
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+
+        });
         final Button btnLogout = v.findViewById(R.id.btnLogout);
 
         btnLogout.setOnClickListener(new View.OnClickListener() {
@@ -42,11 +102,16 @@ public class ProfileFragment extends Fragment {
                 startActivity(new Intent(getActivity(), AuthActivity.class));
             }
         });
-
-
+        FloatingActionButton fab = v.findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Anda Sedang Edit profil", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
 
         return v;
-
     }
 
 }
